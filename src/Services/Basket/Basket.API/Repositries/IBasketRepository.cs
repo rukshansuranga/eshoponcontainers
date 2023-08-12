99e076c5b7da897448ec3d6ad46b77e28720d22e
Rukshan Perera <rukshansuranga@gmail.com>
2023-08-12 10:47:43 +0530

    discount api init

---
+++ src/Services/Basket/Basket.API/Repositries/IBasketRepository.cs
@@ -0,0 +1,10 @@
+using Basket.API.Entities;
+
+namespace Basket.API.Repositries;
+
+public interface IBasketRepository
+{
+    Task<ShoppingCart> GetBasket(string userName);
+    Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+    Task DeleteBasket(string userName);
+}
---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,16 @@
+using Catalog.API.Data;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+public class CatalogContext : ICatalogContext
+{
+    public CatalogContext(IConfiguration configuration)
+    {
+        var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+        
+        Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+        CatalogContextSeed.SeedData(Products);
+    }
+    public IMongoCollection<Product> Products {get;}
+}
